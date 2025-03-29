72ee5d263a811dd9a4e8bbff1f39f1a443348d43
Sniiodromr21 <W172151819@outlook.com>
2025-03-28 21:16:45 -0400

    Update Optimizer.java

--- h2/src/main/org/h2/command/query/Optimizer.java
+++ h2/src/main/org/h2/command/query/Optimizer.java
@@ -13,6 +13,7 @@ import org.h2.table.Plan;
 import org.h2.table.PlanItem;
 import org.h2.table.TableFilter;
 import org.h2.util.Permutations;
+import org.h2.command.query.RuleBasedJoinOrderPicker;
 
 /**
  * The optimizer is responsible to find the best execution plan
@@ -82,7 +83,9 @@ class Optimizer {
         } else {
             startNs = System.nanoTime();
             if (filters.length <= MAX_BRUTE_FORCE_FILTERS) {
-                calculateBruteForceAll(isSelectCommand);
+                RuleBasedJoinOrderPicker ruleBasedJoinOrderPicker = new RuleBasedJoinOrderPicker(session, filters);
+                TableFilter[] ruleBasedResult = ruleBasedJoinOrderPicker.bestOrder();
+                testPlan(ruleBasedResult, isSelectCommand);
             } else {
                 calculateBruteForceSome(isSelectCommand);
                 random = new Random(0);
@@ -110,10 +113,6 @@ class Optimizer {
 
         p.next();
         testPlan(list, isSelectCommand);
-
-//        for (int x = 0; !canStop(x) && p.next(); x++) {
-//            testPlan(list, isSelectCommand);
-//        }
     }
 
     private void calculateBruteForceSome(boolean isSelectCommand) {
